4d2fc41ef2e46001baa42a66be92d42328ddb5d8
lamecksingu <singulameck9619@gmail.com>
2023-07-23 07:17:46 -0700

    Additional declaration of functions

--- main.h
+++ main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(char c);
+int print_string(const char *str);
+int print_integer(int num);
 
 #endif
