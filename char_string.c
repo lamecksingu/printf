8e86970bddff45368478938a9c659eebbbc80a28
Perpet98Sofia <perpetualakwiri@gmail.com>
2023-07-25 15:20:20 -0700

    printf

--- char_string.c
+++ char_string.c
@@ -1,28 +1,73 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * print_char - helper function to print a single character
- * @c: a character to be printed
- * Return: number of characters printed
+ *print_char - Prints character
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
  */
-int print_char(char c)
+int print_char(va_list list)
 {
-	return (write(1, &c, 1));
+	_write_char(va_arg(list, int));
+	return (1);
 }
 
 /**
- * print_string - helper function to print a string
- * @str: a pointer to string
- * Return: number of characters printed
+ * print_string - Prints a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
  */
-int print_string(const char *str)
+int print_string(va_list list)
 {
-	int count = 0;
+	int i;
+	char *str;
 
-	while (*str)
-	{
-		count += write(1, str, 1);
-		str++;
-	}
-	return (count);
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		_write_char(str[i]);
+	return (i);
+}
+
+/**
+ * print_percent - Prints a percent symbol
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_percent(__attribute__((unused))va_list list)
+{
+	_write_char('%');
+	return (1);
+}
+
+/**
+ * print_integer - Prints an integer
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
+ */
+int print_integer(va_list list)
+{
+	int num_length;
+
+	num_length = print_number(list);
+	return (num_length);
+}
+
+/**
+ * unsigned_integer - Prints Unsigned integers
+ * @list: List of all of the argumets
+ * Return: a count of the numbers
+ */
+int unsigned_integer(va_list list)
+{
+	unsigned int num;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (print_unsgined_number(num));
+
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
 }
